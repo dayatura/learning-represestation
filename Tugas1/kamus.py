32e6f830175b9a9ea49e8110769596f1554b6ec9
dayatura <hidayaturrahman7@gmail.com>
2019-01-22 10:03:16 +0700

    add search func

--- Tugas1/kamus.py
+++ Tugas1/kamus.py
@@ -92,9 +92,47 @@ class Kamus:
                     print('  ', end='')
             
             self.right.printKamus(deep)
-    
-    def print(self):
+
+            
+    def printKamus2(self, deep = 0, temp=[], verbose='False', reset=False, words=[]):
+
+        # if reset:
+        #     words=[]
+        #     print(words)
+
+        if verbose == 'True':
+            print(self.data, end='')
+        if self.isLeaf():
+            # print()
+            # print(''.join(temp) + self.data)
+            # print(temp)
+            words.append(''.join(temp) + self.data)
+            # print(words, 'ini temp')
+            # print(words)
+
+        if self.left:
+            deep += 1
+            # print('',end='')
+            temp.append(self.data)
+            self.left.printKamus2(deep,temp,verbose)
+            deep -= 1
+            temp.pop()
+
+        if self.right:
+            
+            if verbose == 'True':
+                print()
+                print(''.join(temp), end ='')
+            self.right.printKamus2(deep, temp,verbose)
+        
+        return words
+
+    def printSturcture(self):
         self.printKamus()
+        print()
+
+    def print(self):
+        self.printKamus2(verbose='True')
         print()
     
     def search(self, keyWord):
@@ -105,11 +143,26 @@ class Kamus:
         
         ketemu = False
 
+        matchWord=[]
+
         for i in range(lenKeyword):
+            
+            # print(self.data)
+            
 
             if keyWord[i] == self.data:
                 # print('kiri')
-                self = self.left
+                if i == lenKeyword-1 and self.isLeaf():
+                    ketemu = True
+                    # print(self.data)
+                    # if not self.isLeaf:
+                    #     ketemu = False
+                elif i == lenKeyword-1:
+                    # hitam = []
+                    ayam = self.left.printKamus2(reset=True)
+                    # print(ayam)
+                else:
+                    self = self.left
 
             elif ord(keyWord[i]) > ord(self.data) and self.right != None:
                 # print('kanan')
@@ -119,23 +172,79 @@ class Kamus:
                 if self.data == keyWord[i]:
                     self = self.left
                     
-            
-            if i == lenKeyword-1:
-                ketemu = True
-                matchWord = []
-                tree2word(self, matchWord, 0)
+            # print(self.data)
+            # if i == lenKeyword-1:
+            #     ketemu = True
+            #     if not self.isLeaf:
+            #         ketemu = False
+            #     if self is not None:
+            #         matchWord = self.printKamus2(verbose=False)
+            #     # tree2word(self, matchWord, 0)
+            #     # matchWord = self.tree2word()
 
 
         if ketemu:
             print('\''+keyWord + '\' ditemukan')
-            print('kata yang mungkin terkait: ')
-            return [ keyWord + x for x in matchWord]
+            # if matchWord != []:
+            #     print('kata yang mungkin terkait: ')
+            #     print([ keyWord + x for x in matchWord])
 
         else:
-            return 'Pencarian tidak ditemukan'
+            print('Pencarian tidak ditemukan')
+            # print([ keyWord + x for x in matchWord])
+            if matchWord != []:
+                print('kata yang mungkin terkait: ')
+                print([ keyWord + x for x in matchWord])
 
+    def isLeaf(self):
+        return self.right is None and self.left is None
+
+    def tree2word(self):
+        words = []
+        stackWord = []
+        temp = Kamus(self.data)
+        temp.right = self.right
+        stackWord.append(self)
+
+        while stackWord != []:
+            # if stackWord[-1].data == 'r':
+                # print('r here')
+            # print('kata saat ini ' + stackWord[-1].data)
+            if stackWord[-1].isLeaf():
+                newWord = []
+                for i in range(len(stackWord)):
+                    newWord.append(stackWord[i].data)
+                words.append(''.join(newWord))
+                print(''.join(newWord))
+                #TODO pop until rigth
+                while stackWord[-1].isLeaf():
+                    stackWord.pop()
+                    print('pop')
+                stackWord[-1].left = None
+            elif stackWord[-1].left:
+                print('kiri')
+                temp = Kamus(stackWord[-1].left.data)
+                temp.right = stackWord[-1].left.right
+                print(temp.left)
+                stackWord.append(stackWord[-1].left)
+            elif stackWord[-1].right:
+                print('kanan')
+                stackWord.append(stackWord[-1].right)
             
+            print(len(stackWord))
+            if len(stackWord) == 1:
+                print(stackWord[-1].left, stackWord[-1].data, stackWord[-1].right.data)
+                break
+
+        return words
+        # if self.isLeaf():
+        #     word.append(stackWord.join())
+        # else:
+        #     stackWord.append(self.data)
+        #     self = self.left
             
+    # def tree2word2(self):
+
             
 
 
@@ -217,6 +326,9 @@ kamus.add('bros')
 kamus.add('bulat')
 kamus.add('bubuk')
 kamus.add('bry')
+
+kamus.printSturcture()
+print()
 kamus.print()
 
-print(kamus.search('br'))
+kamus.search('bubar')
